04ca00508b903b06e7030fe367bc33823ea0c053
rawi <rawi.mousa@gmail.com>
2025-03-23 15:39:41 +0200

    Initial commit - whistgame testing

---
+++ Helpers/HelperFunctions.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace WhistGameTests.Helpers
+{
+    internal class HelperFunctions
+    {
+        public static By WaitForButton(IWebDriver player, string button, int time = 3 , string type = "id")
+        {
+            By            locator = type == "id" ? By.Id(button) : By.XPath(button);
+            WebDriverWait wait    = new WebDriverWait(player, TimeSpan.FromSeconds(time));
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(locator));
+            return locator;
+        }
+
+
+        public static void ClickButton(IWebDriver player, string button, int time = 3, string type = "id")
+        {
+            var locator = WaitForButton(player, button, time , type);
+            var readyButton = player.FindElement(locator);
+            readyButton.Click();
+        }
+    }
+}
---
+++ WebDriverFixture .cs
@@ -0,0 +1,58 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+public class WebDriverFixture : IDisposable
+{
+    public IWebDriver ChromeDriver1 { get; private set; }
+    public IWebDriver ChromeDriver2 { get; private set; }
+    public IWebDriver ChromeDriver3 { get; private set; }
+    public IWebDriver ChromeDriver4 { get; private set; }
+
+    public WebDriverFixture()
+    {
+        ChromeDriver1 = CreateUndetectableChromeDriver("Player1");
+        ChromeDriver2 = CreateUndetectableChromeDriver("Player2");
+        ChromeDriver3 = CreateUndetectableChromeDriver("Player3");
+        ChromeDriver4 = CreateUndetectableChromeDriver("Player4");
+    }
+
+    private IWebDriver CreateUndetectableChromeDriver(string playerName)
+    {
+        var options = new ChromeOptions();
+
+        string profilePath = $@"C:\Selenium\ChromeProfiles\{playerName}";
+        options.AddArgument($"--user-data-dir={profilePath}");
+        options.AddArgument("--profile-directory=Default");
+
+
+        options.AddExcludedArgument("enable-automation");
+        options.AddAdditionalOption("useAutomationExtension", false);
+        options.AddArgument("--disable-blink-features=AutomationControlled");
+
+        var driver = new ChromeDriver(options);
+
+        var script = @"
+            Object.defineProperty(navigator, 'webdriver', {
+                get: () => undefined
+            });
+        ";
+
+        ((IJavaScriptExecutor)driver).ExecuteScript(
+            "window.open('','_blank');"
+        );
+        driver.SwitchTo().Window(driver.WindowHandles[1]);
+        ((IJavaScriptExecutor)driver).ExecuteScript(script);
+        driver.Close();
+        driver.SwitchTo().Window(driver.WindowHandles[0]);
+
+        return driver;
+    }
+
+    public void Dispose()
+    {
+        //ChromeDriver1?.Quit();
+        //ChromeDriver2?.Quit();
+        //ChromeDriver3?.Quit();
+        //ChromeDriver4?.Quit();
+    }
+}
